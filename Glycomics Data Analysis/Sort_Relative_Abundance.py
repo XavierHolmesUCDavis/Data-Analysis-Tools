da7a66c6199b937ddfce4dd2cdca9a795fe9cdda
XavierHolmesUCDavis <112580276+XavierHolmesUCDavis@users.noreply.github.com>
2023-10-10 02:01:08 -0700

    Update Sort_Relative_Abundance.py

--- Glycomics Data Analysis/Sort_Relative_Abundance.py
+++ Glycomics Data Analysis/Sort_Relative_Abundance.py
@@ -48,8 +48,8 @@ for i in range(len(unique_files)):
     current_data = df_from_csv[df_from_csv['File'] == unique_files[i]]
     filename = unique_files[i] + '.csv'
     current_data.to_csv(filename, index=False)
-    compound_list = [unique_files[i]] * len(current_data)
-    compound_list_table = pd.DataFrame({'file': compound_list})
+    #compound_list = [unique_files[i]] * len(current_data)
+    #compound_list_table = pd.DataFrame({'file': compound_list})
     with open(filename, 'r+') as f:
         content = f.read()
         f.seek(0, 0)
